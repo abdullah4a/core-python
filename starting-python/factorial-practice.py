1b177735994f002cfc4818af3846c30378c06e38
abdullah4a <abdullahkhan4a@gmail.com>
2023-05-04 23:46:02 +0500

    calculate base of string

--- starting-python/factorial-practice.py
+++ starting-python/factorial-practice.py
@@ -2,8 +2,8 @@ from math import factorial as fact
 
 
 def main():
-    n=5
-    factorial=fact(n)
+    n = 5
+    factorial = fact(n)
     print(f"{factorial}")
     print(len(str(factorial)))
     print("Hacks")
@@ -11,6 +11,12 @@ def main():
     print(0o10)
     print(0x10)
     print(int(3.5))
-
+    """
+    To take base of variable
+    one need to pass int(str_val, base)
+    eg int("10000",3) // output will be 81
+    see example below
+    """
+    print(int("10000",3))
 
 main()
