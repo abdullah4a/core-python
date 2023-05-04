936f2907a074b547f5f3fa569d5c933ae6e032dd
abdullah4a <abdullahkhan4a@gmail.com>
2023-05-04 23:41:58 +0500

    cool hacks

--- starting-python/factorial-practice.py
+++ starting-python/factorial-practice.py
@@ -6,6 +6,11 @@ def main():
     factorial=fact(n)
     print(f"{factorial}")
     print(len(str(factorial)))
+    print("Hacks")
+    print(0b10)
+    print(0o10)
+    print(0x10)
+    print(int(3.5))
 
 
 main()
